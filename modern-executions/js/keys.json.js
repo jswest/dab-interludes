d5fc1defb11938f6fca946c1bdca9a3faadd8fb5
John West <john@mmabanta537431.att.net>
2014-10-04 10:58:42 -0500

    refactors the modern-executions code.

---
+++ modern-executions/js/keys.json.js
@@ -0,0 +1,31 @@
+var keys = {
+  race: {
+    'white': { 'name': 'white', 'index': 0 },
+    'black': { 'name': 'black', 'index': 1 },
+    'latino': { 'name': 'latino', 'index': 2 },
+    'native american': {'name': 'native american', 'index': 3},
+    'asian': {'name': 'asian', 'index': 4 },
+    'other': {'name': 'other', 'index': 5 }
+  },
+  method: {
+    'lethal injection': { 'name': 'lethal injection', 'index': 0 },
+    'electrocution': { 'name': 'electrocution', 'index': 1 },
+    'gas chamber': { 'name': 'gas chamber', 'index': 2 },
+    'firing squad': { 'name': 'firing squad', 'index': 3 },
+    'hanging': { 'name': 'hanging', 'index': 4 }
+  },
+  region: {
+    's': { 'name': 'south', 'index': 0 },
+    'n': { 'name': 'northeast', 'index': 1 },
+    'm': { 'name': 'midwest', 'index': 2 },
+    'w': { 'name': 'west', 'index': 3 }
+  },
+  sex: {
+    'm': { 'name': 'male', 'index': 0 },
+    'f': { 'name': 'female', 'index': 1 }
+  },
+  juvenile: {
+    'no': { 'name': 'adult', 'index': 0 },
+    'yes': { 'name': 'juvenlie', 'index': 1 }
+  }
+};
--- modern-executions/js/modern-executions.js
+++ modern-executions/js/modern-executions.js
@@ -1,366 +1,238 @@
-DAB.ModernExecutions = function (el) {
+DAB.ModernExecutions = function ($el) {
 
   var that = this;
 
-  var sortKey = 'race';
 
+
+  // Define a function that will create the controls for the unit.
   var createControls = function () {
-    el.append(
-      '<ul class="controls">' +
-        '<li class="control" data-sort-key="method">method</li>' +
-        '<li class="control" data-sort-key="region">region</li>' +
-        '<li class="control active" data-sort-key="race">race</li>' +
-        '<li class="control" data-sort-key="juvenile">juvenile</li>' +
-        '<li class="control" data-sort-key="sex">sex</li>' +
-      '</ul>'
-    );
-    el.find('ul.controls').on('click', function (e) {
-      $(this).toggleClass('clicked');
-    });
-  };
-
-  var keys = {
-    "race": {
-      "white": {
-        "nicename": "white",
-        "colorIndex": 0
-      },
-      "black": {
-        "nicename": "black",
-        "colorIndex": 1
-      },
-      "latino": {
-        "nicename": "latino",
-        "colorIndex": 2
-      },
-      "native american": {
-        "nicename": "native american",
-        "colorIndex": 3
-      },
-      "asian": {
-        "nicename": "asian",
-        "colorIndex": 4
-      },
-      "other": {
-        "nicename": "other",
-        "colorIndex": 5
-      }
-    },
-    "method": {
-      "lethal injection": {
-        "nicename": "lethal injection",
-        "colorIndex": 0
-      },
-      "electrocution": {
-        "nicename": "electrocution",
-        "colorIndex": 1
-      },
-      "gas chamber": {
-        "nicename": "gas chamber",
-        "colorIndex": 2
-      },
-      "firing squad": {
-        "nicename": "firing squad",
-        "colorIndex": 3
-      },
-      "hanging": {
-        "nicename": "hanging",
-        "colorIndex": 4
-      }
-    },
-    "region": {
-      "s": {
-        "nicename": "south",
-        "colorIndex": 0
-      },
-      "n": {
-        "nicename": "northeast",
-        "colorIndex": 1
-      },
-      "m": {
-        "nicename": "midwest",
-        "colorIndex": 2
-      },
-      "w": {
-        "nicename": "west",
-        "colorIndex": 3
-      }
-    },
-    "sex": {
-      "m": {
-        "nicename": "male",
-        "colorIndex": 0
-      },
-      "f": {
-        "nicename": "female",
-        "colorIndex": 1
-      }
-    },
-    "juvenile": {
-      "no": {
-        "nicename": "adult",
-        "colorIndex": 0
-      },
-      "yes": {
-        "nicename": "juvenlie",
-        "colorIndex": 1
-      }
+    // If we haven't yet created the controls, only then create and bind them.
+    if ($el.find('ul.controls').length === 0) {
+      $el.append(
+        '<ul class="controls">' +
+          '<li class="control method-control" data-sort-key="method">method</li>' +
+          '<li class="control region-control" data-sort-key="region">region</li>' +
+          '<li class="control race-control" data-sort-key="race">race</li>' +
+          '<li class="control juvenile-control" data-sort-key="juvenile">juvenile</li>' +
+          '<li class="control sex-control" data-sort-key="sex">sex</li>' +
+        '</ul>'
+      );
+      $el.find('ul.controls').on('click', function (e) {
+        $(this).toggleClass('clicked');
+      });
     }
+  };
+
+  // Define a function that will create the legend for the unit.
+  var createLegend = function (key) {
+    // Create the legend element if it hasn't been created before.
+    if ($el.find('table.legend').length === 0) {
+      $el.append('<table class="legend"></table>');
+    }
+    var $legend = $el.find('table.legend');
+    $legend.empty();
+    var segments = d3.keys(keys[key]); // Find the segments we'll be including in the legend.
+    for (var i = 0; i < segments.length; i++) {
+      // As we iterate over the segments, we're adding a color-block that matches up to the color of the rects in the viz.
+      // We're also adding in the display name of the segment.
+      $legend.append(
+        '<tr>' +
+          '<td style="background-color:' + c(keys[key][segments[i]].index) + ';" class="colorblock"></td>' +
+          '<td>' + keys[key][segments[i]].name + '</td>' +
+        '</tr>'
+      );
+    }
+  };
+
+  var createInspector = function (e, d3el) {
+    var d = d3.select(d3el).datum();
+    if ($el.find('.inspector').length === 0) {
+      $el.append(
+        '<table class="inspector">' +
+          '<tr>' +
+            '<th>name</th>' +
+            '<td class="inspector-name">' + d.name + '</td>' + 
+          '</tr>' +
+          '<tr>' +
+            '<th>method</th>' +
+            '<td class="inspector-method">' + d.method + '</td>' + 
+          '</tr>' +
+          '<tr>' +
+            '<th>race</th>' +
+            '<td class="inspector-race">' + d.race + '</td>' + 
+          '</tr>' +
+          '<tr>' +
+            '<th>sex</th>' +
+            '<td class="inspector-sex">' + d.sex + '</td>' + 
+          '</tr>' +
+          '<tr>' +
+            '<th>state</th>' +
+            '<td class="inspector-state">' + d.state + '</td>' + 
+          '</tr>' +
+        '</table>'
+      );
+
+      var $inspector = $el.find('.inspector');      
+      var transformAmount = (e.clientY - $el.offset().top > $(window).height() / 2) ? -$inspector.height() - 20 : 20
+      $inspector.css({
+        'top': e.clientY - $el.offset().top + 20 + transformAmount,
+        'left': e.clientX - $el.offset().left - ($inspector.width() / 2)
+      });
+    }
+  };
+
+
+  /* Define some utility functions. */
 
 
 
-  }
-
-  var sort = function (data, sortKey) {
+  // Define a function that will sort data based on its key's index.
+  var sort = function (data, key) {
     return data.sort(function (a, b) {
-      return keys[sortKey][a[sortKey]].colorIndex > keys[sortKey][b[sortKey]].colorIndex ? 1 : -1;
+      // This is super readable, I know...
+      // Compare based on the index in the keys variable (defined in the keys.json.js file).
+      return keys[key][a[key]].index > keys[key][b[key]].index ? 1 : -1;
     });
-    /*return data.sort(function (a, b) {
-      return colorIndex[a[sortKey]] > colorIndex[b[sortKey]] ? 1 : -1
-    });*/
   };
 
-  var year = function (date) {
-    return new Date(date.split('/')[2] + '-01-01');
+
+
+  // Define a function that will grab the year from the execution's date.
+  // This is useful as we'll use that year to stack the data in some views.
+  // It takes a date in the format dd/mm/year and returns just the year.
+  //var year = function (date) { return new Date(date.split('/')[2] + '-01-01'); };
+  var year = function (date) { return date.split('/')[2]; };
+
+
+
+
+
+  var margin = { top: 22, right: 22, bottom: 33, left: 33 }
+  ,   width  = $(window).width() - margin.left - margin.right
+  ,   height = $(window).height() - margin.top - margin.bottom;
+
+  var svg = d3.select($el[0]).append('svg')
+    .attr({ width: width + margin.left + margin.right, height: height + margin.top + margin.bottom })
+    .append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+
+
+
+
+  // Define the color scale.
+  var colors = [ 'rgb(255,149,207)', 'rgb(255,0,174)', 'rgb(255,207,149)', 'rgb(255,174,0)', 'rgb(149,207,255)', 'rgb(0,174,255)' ];
+  //var c = d3.scale.ordinal().domain([5,4,3,2,1,0]).range(colors);
+  var c = d3.scale.category20();
+
+  // Define the x scale; we'll need all the representative years in the data.
+  var years = [];
+  var yearTicks = []; // Define the yearTicks array, which will be used to show only some of the ticks.
+  for (var i = 1977; i <= 2014; i++) {
+    years.push('' + i); // Cast it as a string.
+    if ((i - 1977) % 4 === 0) {
+      yearTicks.push('' + i)
+    } else {
+      yearTicks.push('');
+    }
+  }
+  var x = d3.scale.ordinal().domain(years).rangeBands([0, width], 0);
+  var y = d3.scale.linear().domain([0, 100]).range([height, 0]);
+  var h = d3.scale.linear().domain([0, 100]).range([0, height]);
+
+
+  // Create the x axis
+  var xAxis = d3.svg.axis().scale(x).orient('bottom').tickValues(yearTicks);
+  svg.append('g').attr('class', 'axis x-axis');
+  d3.select('g.x-axis').call(xAxis)
+    .attr('transform', 'translate(0,' + height + ')');
+    //.selectAll('text').style('transform', 'rotate(90deg)');
+
+  // Create the y axis
+  var yAxis = d3.svg.axis().scale(y).orient('left');
+  svg.append('g').attr('class', 'axis y-axis');
+  d3.select('g.y-axis').call(yAxis);
+
+  var create = function (data, key) {
+    createLegend(key);
+    var indices = {};
+    var rect = svg.selectAll('rect')
+      .data(sort(data, key))
+      .enter()
+      .append('rect')
+      .attr('transform', function (d, i) {
+        return 'translate(' + x(year(d.date)) + ',' + height + ')';
+      })
+      .transition()
+      .delay(500)
+      .duration(700)
+      .attr('width', x.rangeBand() - 1)
+      .attr('height', h(1) - 1)
+      .attr('transform', function (d, i) {
+        var date = year(d.date).toString(); 
+        if ( indices[date] ) {
+          indices[date]++;
+        } else {
+          indices[date] = 1;
+        }
+        return 'translate(' + x(date) + ',' + y(indices[date]) + ')';
+      })
+      .style( 'fill', function (d) { return c(keys[key][d[key]].index); } );
+    $('rect').on('mouseenter', function (e) {
+      createInspector(e, this);
+    }).on('mouseleave', function (e) {
+      $('.inspector').remove();
+    })
   };
 
-  var sizes
-  ,   svg
-  ,   x
-  ,   y
-  ,   h
-  ,   color
-  ,   oneYearsWidth;
-  this.build = function () {
+
+  var update = function (data, key) {
+    createLegend(key);
+    var indices = {};
+    var rect = svg.selectAll('rect')
+      .transition()
+      .duration(500)
+      .attr('transform', function (d, i) {
+        return 'translate(' + x(year(d.date)) + ',' + height + ')';
+      });
+    rect = svg.selectAll('rect')
+      .data(sort(data, key))
+      .transition()
+      .delay(500)
+      .duration(100)
+      .attr('transform', function (d, i) {
+        return 'translate(' + x(year(d.date)) + ',' + height + ')';
+      })
+      .transition()
+      .delay(600)
+      .duration(500)
+      .attr('transform', function (d, i) {
+        var date = year(d.date).toString(); 
+        if ( indices[date] ) {
+          indices[date]++;
+        } else {
+          indices[date] = 1;
+        }
+        return 'translate(' + x(date) + ',' + y(indices[date]) + ')';
+      })
+      .style( 'fill', function (d) { return c(keys[key][d[key]].index); } );
+  };
+
+
+  d3.json('data/dpic.json', function (data) {
+    create(data, 'race');
 
     createControls();
-
-    var sizes = {
-      elWidth: el.width(),
-      elHeight: $(window).height() - 44,
-      width: el.width() - 88, // this shouldn't be hardcoded. we should have a sizes global
-      height: $(window).height() - 132,
-      top: 44,
-      left: 44
-    };
-
-    var svg = d3.select(el[0]).append('svg')
-      .attr('width', sizes.elWidth)
-      .attr('height', sizes.elHeight);
-
-    d3.json('data/dpic.json', function (data) {
-      
-      var minx = d3.min( data, function ( d ) { return year(d.date); } )
-      //,   maxx = d3.max( data, function ( d ) { return year(d.date); } )
-      ,   maxx = new Date('2015')
-      ,   miny = 0
-      ,   maxy = 100;
-
-      x = d3.time.scale()
-        .domain([minx, maxx])
-        .range([sizes.left, sizes.left + sizes.width]);
-      y = d3.scale.linear()
-        .domain([maxy, miny])
-        .range([sizes.top, sizes.top + sizes.height]);
-      h = d3.scale.linear()
-        .domain([miny, maxy])
-        .range([0, sizes.height]);
-      var colors = [
-        'rgb(255,149,207)',
-        'rgb(255,0,174)',
-        'rgb(255,207,149)',
-        'rgb(255,174,0)',
-        'rgb(149,207,255)',
-        'rgb(0,174,255)'
-      ];
-      var color = d3.scale.ordinal()
-        .domain([5,4,3,2,1,0])
-        .range(colors)
-      oneYearsWidth = x( new Date('2014-01-01')) - x( new Date('2013-01-01'));
-
-      var createKey = function (sortKey) {
-        if (el.find('table.key').length === 0) {
-          el.append('<table class="key"></table>');
-        }
-        var keyel = el.find('table.key')
-        keyel.html('');
-        var segments = _.keys(keys[sortKey]);
-        for (var i = 0; i < segments.length; i++) {
-          keyel.append(
-            '<tr>' +
-              '<td style="background-color:' + color(keys[sortKey][segments[i]].colorIndex) + ';" class="colorblock"></td>' +
-              '<td>' + keys[sortKey][segments[i]].nicename + '</td>' +
-            '</tr>'
-          );
-        }
-      };
-
-      var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient('left')
-        .ticks(10)
-        .tickSize(-sizes.width)
-      svg.append('g').attr('class', 'y-axis')
-      svg.select('g.y-axis')
-        .call(yAxis)
-        .attr('transform', 'translate(44,0)');
-
-      var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient('bottom')
-        .ticks(10)
-      svg.append('g').attr('class', 'x-axis')
-      svg.select('g.x-axis')
-        .call(xAxis)
-        .attr('transform', 'translate(0,' + (sizes.height + 44) + ')')
-        .selectAll('text')
-        .attr('transform', 'translate(' + (oneYearsWidth / 2) + ',0)');
-
-      var rect = svg.selectAll('rect')
-        .data(sort(data, sortKey))
-        .enter()
-        .append('rect');
-
-      var create = function (sortKey) {
-        createKey(sortKey);
-        var indices = {};
-        rect
-          .attr('transform', function (d, i) {
-            var xpos = x(year(d.date));
-            var ypos = sizes.height + sizes.top;
-            return 'translate(' + xpos + ',' + ypos + ')';
-          })
-          .transition()
-          .delay(500)
-          .duration(700)
-          .attr('width', oneYearsWidth - 2)
-          .attr('height', h(1) - 2)
-          .attr('transform', function (d, i) {
-            var date = year(d.date).toString(); 
-            if ( indices[date] ) {
-              indices[date]++;
-            }
-            else {
-              indices[date] = 1;
-            }
-            var xpos = x(year(d.date));
-            var ypos = y(indices[date]);
-            return 'translate(' + xpos + ',' + ypos + ')';
-          })
-          .style( 'fill', function (d, i) { return color(keys[sortKey][d[sortKey]].colorIndex); } );
-        el.find('rect')
-          .on('mouseover', function (e) {
-            var d = d3.select(this).datum();
-            var name    = d.name
-            ,   method  = d.method
-            ,   race    = d.race
-            ,   sex     = d.sex 
-            ,   state   = d.state;
-            if (el.find('.inspector').length === 0) {
-              el.append(
-                '<table class="inspector">' +
-                  '<tr>' +
-                    '<th>name</th>' +
-                    '<td class="inspector-name">' + name + '</td>' + 
-                  '</tr>' +
-                  '<tr>' +
-                    '<th>method</th>' +
-                    '<td class="inspector-method">' + method + '</td>' + 
-                  '</tr>' +
-                  '<tr>' +
-                    '<th>race</th>' +
-                    '<td class="inspector-race">' + race + '</td>' + 
-                  '</tr>' +
-                  '<tr>' +
-                    '<th>sex</th>' +
-                    '<td class="inspector-sex">' + sex + '</td>' + 
-                  '</tr>' +
-                  '<tr>' +
-                    '<th>state</th>' +
-                    '<td class="inspector-state">' + state + '</td>' + 
-                  '</tr>' +
-                '</table>'
-              );
-            } else {
-              var inspector = el.find('.inspector');
-            }
-            var inspector = el.find('.inspector');
-            
-            if (e.clientY - el.offset().top > $(window).height() / 2) {
-              var transformAmount = -inspector.height() - 20;
-            } else {
-              var transformAmount = 20;
-            }
-
-            inspector.css({
-              'top': e.clientY - el.offset().top + 20 + transformAmount,
-              'left': e.clientX - el.offset().left - (inspector.width() / 2)
-            });
-          })
-          .on('mouseout', function (e) {
-            el.find('.inspector').remove();
-          })
-      };
-
-      var update = function (sortKey) {
-        createKey(sortKey);
-        var indices = {};
-        rect
-          .transition()
-          .duration(500)
-          .attr('transform', function (d, i) {
-            var xpos = x(year(d.date));
-            var ypos = sizes.height + sizes.top;
-            return 'translate(' + xpos + ',' + ypos + ')';
-          })
-        rect
-          .data(sort(data, sortKey))
-          .transition()
-          .delay(500)
-          .duration(100)
-          .attr('transform', function (d, i) {
-            var xpos = x(year(d.date));
-            var ypos = sizes.height + sizes.top;
-            return 'translate(' + xpos + ',' + ypos + ')';
-          })
-          .transition()
-          .delay(600)
-          .duration(500)
-          .attr('transform', function (d, i) {
-            var date = year(d.date).toString(); 
-            if ( indices[date] ) {
-              indices[date]++;
-            }
-            else {
-              indices[date] = 1;
-            }
-            var xpos = x(year(d.date));
-            var ypos = y(indices[date]);
-            return 'translate(' + xpos + ',' + ypos + ')';
-          })
-          .style( 'fill', function (d, i) { return color(keys[sortKey][d[sortKey]].colorIndex); } );
-      };
-
-      el.find('.control').on('click', function (e) {
-        if (!$(this).hasClass('active')) {
-          el.find('.control').removeClass('active');
-          $(this).addClass('active');
-          var newSortKey = $(this).data('sort-key');
-          update(newSortKey);          
-        }   
-      });
-
-      create(sortKey);
+    $el.find('.control').on('click', function (e) {
+      if (!$(this).hasClass('active')) {
+        $el.find('.control').removeClass('active');
+        $(this).addClass('active');
+        var newKey = $(this).data('sort-key');
+        update(data, newKey);          
+      }   
     });
 
-  };
-
-
-  
-
-
-  this.destroy = function () {
-    el.find('svg').remove();
-  };
+  });
 
 };
+
+
